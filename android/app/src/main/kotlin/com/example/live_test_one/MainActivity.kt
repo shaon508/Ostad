a9a2a27ab3e40cf02fb1a7da2bf9595be78852ec
shaon508 <shaonroy54@gmail.com>
2025-01-01 23:30:46 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/live_test_one/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.live_test_one
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'dart:ffi';
+import 'dart:io';
+
+double triangle(double b, double h) {
+  return 0.5 * b * h;
+}
+
+main() {
+  double? base = double.tryParse(stdin.readLineSync()!);
+  double? height = double.tryParse(stdin.readLineSync()!);
+  if (base != null && height != null) {
+    double area = triangle(base, height);
+    print("Area is $area");
+  } else {
+    print("Invaild Input!");
+  }
+}
